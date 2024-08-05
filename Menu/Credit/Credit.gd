6301fec01f3522a54e0b7f3d03a1f348fab015f3
Bauke Conijn <bcmpinc@gmail.com>
2024-08-05 03:06:53 +0200

    Credit tags

---
+++ Menu/Credit/Credit.gd
@@ -0,0 +1,27 @@
+@tool
+extends TextureButton
+
+@export var author_image: Texture2D:
+	set(new):
+		author_image = new
+		if $AuthorImage:
+			$AuthorImage.texture = new
+@export var author_name: String:
+	set(new):
+		author_name = new
+		if $AuthorName:
+			$AuthorName.text = new
+@export_color_no_alpha var background_color: Color:
+	set(new):
+		background_color = new
+		if $Background:
+			$Background.color = new
+@export var link: String
+
+func _ready():
+	$AuthorImage.texture = author_image
+	$AuthorName.text = author_name
+	$Background.color = background_color
+	self.pressed.connect(func():
+		OS.shell_open(link)
+	)
