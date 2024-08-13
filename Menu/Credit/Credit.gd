60657f130fe38b7282d33babf83ac4b8237d4b99
Bauke Conijn <bcmpinc@gmail.com>
2024-08-13 02:08:44 +0200

    Use clipping mask for credits

--- Menu/Credit/Credit.gd
+++ Menu/Credit/Credit.gd
@@ -4,24 +4,24 @@ extends TextureButton
 @export var author_image: Texture2D:
 	set(new):
 		author_image = new
-		if $AuthorImage:
-			$AuthorImage.texture = new
+		if $ClipMask/AuthorImage:
+			$ClipMask/AuthorImage.texture = new
+@export_color_no_alpha var background_color: Color:
+	set(new):
+		background_color = new
+		if $ClipMask/Background:
+			$ClipMask/Background.color = new
 @export var author_name: String:
 	set(new):
 		author_name = new
 		if $AuthorName:
 			$AuthorName.text = new
-@export_color_no_alpha var background_color: Color:
-	set(new):
-		background_color = new
-		if $Background:
-			$Background.color = new
 @export var link: String
 
 func _ready():
-	$AuthorImage.texture = author_image
+	$ClipMask/AuthorImage.texture = author_image
+	$ClipMask/Background.color = background_color
 	$AuthorName.text = author_name
-	$Background.color = background_color
 	self.pressed.connect(func():
 		Global.click()
 		OS.shell_open(link)
